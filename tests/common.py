a002852aacef43ccf33e7e79cb4ed77b5e03cf81
rsb-23 <57601627+rsb-23@users.noreply.github.com>
2024-11-05 22:47:58 +0530

    Added tests from txt file

--- tests/common.py
+++ tests/common.py
@@ -5,11 +5,11 @@ TEST_FILE_DIR = "tests/test_files"
 two_hours = dt.timedelta(hours=2)
 
 
-def get_test_file(path):
+def get_test_file(file_name: str) -> str:
     """
     Helper function to open and read test files.
     """
-    filepath = f"{TEST_FILE_DIR}/{path}"
+    filepath = f"{TEST_FILE_DIR}/{file_name}"
     with open(filepath, "r", encoding="utf-8") as f:
         text = f.read()
     return text
