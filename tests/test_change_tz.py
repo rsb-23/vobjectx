489a460d81b1dccef3c4c5678e413e54e23f717c
rsb-23 <57601627+rsb-23@users.noreply.github.com>
2025-02-03 01:21:15 +0530

    Pytest changes p1

--- tests/test_change_tz.py
+++ tests/test_change_tz.py
@@ -1,5 +1,4 @@
 import datetime as dt
-import unittest
 from dataclasses import dataclass
 
 from dateutil.tz import gettz
@@ -24,78 +23,77 @@ class StubCal:  # pylint:disable=R0903
         self.vevent_list = [StubEvent(*d) for d in dates]
 
 
-class TestChangeTZ(unittest.TestCase):
-    """Tests for change_tz.change_tz"""
+def test_change_tz():
+    """Change the timezones of events in a component to a different timezone"""
+    # Setup - create a stub vevent list
+    old_tz = gettz("UTC")  # 0:00
+    new_tz = gettz("America/Chicago")  # -5:00
 
-    def test_change_tz(self):
-        """Change the timezones of events in a component to a different timezone"""
-        # Setup - create a stub vevent list
-        old_tz = gettz("UTC")  # 0:00
-        new_tz = gettz("America/Chicago")  # -5:00
+    dates = [
+        (dt.datetime(1999, 12, 31, 23, 59, 59, tzinfo=old_tz), dt.datetime(2000, 1, 1, tzinfo=old_tz)),
+        (dt.datetime(2010, 12, 31, 23, 59, 59, tzinfo=old_tz), dt.datetime(2011, 1, 2, 3, tzinfo=old_tz)),
+    ]
 
-        dates = [
-            (dt.datetime(1999, 12, 31, 23, 59, 59, tzinfo=old_tz), dt.datetime(2000, 1, 1, tzinfo=old_tz)),
-            (dt.datetime(2010, 12, 31, 23, 59, 59, tzinfo=old_tz), dt.datetime(2011, 1, 2, 3, tzinfo=old_tz)),
-        ]
+    cal = StubCal(dates)
 
-        cal = StubCal(dates)
+    # Exercise - change the timezone
+    change_tz(cal, new_tz, gettz("UTC"))
 
-        # Exercise - change the timezone
-        change_tz(cal, new_tz, gettz("UTC"))
+    # Test - that the tzs were converted correctly
+    expected_new_dates = [
+        (dt.datetime(1999, 12, 31, 17, 59, 59, tzinfo=new_tz), dt.datetime(1999, 12, 31, 18, tzinfo=new_tz)),
+        (dt.datetime(2010, 12, 31, 17, 59, 59, tzinfo=new_tz), dt.datetime(2011, 1, 1, 21, tzinfo=new_tz)),
+    ]
 
-        # Test - that the tzs were converted correctly
-        expected_new_dates = [
-            (dt.datetime(1999, 12, 31, 17, 59, 59, tzinfo=new_tz), dt.datetime(1999, 12, 31, 18, tzinfo=new_tz)),
-            (dt.datetime(2010, 12, 31, 17, 59, 59, tzinfo=new_tz), dt.datetime(2011, 1, 1, 21, tzinfo=new_tz)),
-        ]
+    for vevent, expected_datepair in zip(cal.vevent_list, expected_new_dates):
+        assert vevent.dtstart.value == expected_datepair[0]
+        assert vevent.dtend.value == expected_datepair[1]
 
-        for vevent, expected_datepair in zip(cal.vevent_list, expected_new_dates):
-            self.assertEqual(vevent.dtstart.value, expected_datepair[0])
-            self.assertEqual(vevent.dtend.value, expected_datepair[1])
 
-    def test_change_tz_utc_only(self):
-        """Change any UTC timezones of events in a component to a different timezone"""
+def test_change_tz_utc_only():
+    """Change any UTC timezones of events in a component to a different timezone"""
 
-        # Setup - create a stub vevent list
-        utc_tz = gettz("UTC")  # 0:00
-        non_utc_tz = gettz("America/Santiago")  # -4:00
-        new_tz = gettz("America/Chicago")  # -5:00
+    # Setup - create a stub vevent list
+    utc_tz = gettz("UTC")  # 0:00
+    non_utc_tz = gettz("America/Santiago")  # -4:00
+    new_tz = gettz("America/Chicago")  # -5:00
 
-        dates = [(dt.datetime(1999, 12, 31, 23, 59, 59, tzinfo=utc_tz), dt.datetime(2000, 1, 1, tzinfo=non_utc_tz))]
+    dates = [(dt.datetime(1999, 12, 31, 23, 59, 59, tzinfo=utc_tz), dt.datetime(2000, 1, 1, tzinfo=non_utc_tz))]
 
-        cal = StubCal(dates)
+    cal = StubCal(dates)
 
-        # Exercise - change the timezone passing utc_only=True
-        change_tz(cal, new_tz, gettz("UTC"), utc_only=True)
+    # Exercise - change the timezone passing utc_only=True
+    change_tz(cal, new_tz, gettz("UTC"), utc_only=True)
 
-        # Test - that only the utc item has changed
-        expected_new_dates = [(dt.datetime(1999, 12, 31, 17, 59, 59, tzinfo=new_tz), dates[0][1])]
+    # Test - that only the utc item has changed
+    expected_new_dates = [(dt.datetime(1999, 12, 31, 17, 59, 59, tzinfo=new_tz), dates[0][1])]
 
-        for vevent, expected_datepair in zip(cal.vevent_list, expected_new_dates):
-            self.assertEqual(vevent.dtstart.value, expected_datepair[0])
-            self.assertEqual(vevent.dtend.value, expected_datepair[1])
+    for vevent, expected_datepair in zip(cal.vevent_list, expected_new_dates):
+        assert vevent.dtstart.value == expected_datepair[0]
+        assert vevent.dtend.value == expected_datepair[1]
 
-    def test_change_tz_default(self):
-        """
-        Change the timezones of events in a component to a different timezone, passing a default timezone that is
-        assumed when the events don't have one
-        """
 
-        # Setup - create a stub vevent list
-        new_tz = gettz("America/Chicago")  # -5:00
+def test_change_tz_default():
+    """
+    Change the timezones of events in a component to a different timezone, passing a default timezone that is
+    assumed when the events don't have one
+    """
 
-        dates = [(dt.datetime(1999, 12, 31, 23, 59, 59, tzinfo=None), dt.datetime(2000, 1, 1, tzinfo=None))]
+    # Setup - create a stub vevent list
+    new_tz = gettz("America/Chicago")  # -5:00
 
-        cal = StubCal(dates)
+    dates = [(dt.datetime(1999, 12, 31, 23, 59, 59, tzinfo=None), dt.datetime(2000, 1, 1, tzinfo=None))]
 
-        # Exercise - change the timezone
-        change_tz(cal, new_tz, gettz("UTC"))
+    cal = StubCal(dates)
 
-        # Test - that the tzs were converted correctly
-        expected_new_dates = [
-            (dt.datetime(1999, 12, 31, 17, 59, 59, tzinfo=new_tz), dt.datetime(1999, 12, 31, 18, tzinfo=new_tz))
-        ]
+    # Exercise - change the timezone
+    change_tz(cal, new_tz, gettz("UTC"))
 
-        for vevent, expected_datepair in zip(cal.vevent_list, expected_new_dates):
-            self.assertEqual(vevent.dtstart.value, expected_datepair[0])
-            self.assertEqual(vevent.dtend.value, expected_datepair[1])
+    # Test - that the tzs were converted correctly
+    expected_new_dates = [
+        (dt.datetime(1999, 12, 31, 17, 59, 59, tzinfo=new_tz), dt.datetime(1999, 12, 31, 18, tzinfo=new_tz))
+    ]
+
+    for vevent, expected_datepair in zip(cal.vevent_list, expected_new_dates):
+        assert vevent.dtstart.value == expected_datepair[0]
+        assert vevent.dtend.value == expected_datepair[1]
--- tests/test_win32tz.py
+++ tests/test_win32tz.py
@@ -1,25 +1,28 @@
 import datetime as dt
 import sys
-from unittest import TestCase, skip, skipUnless
+
+import pytest
 
 if sys.platform == "win32":
     from vobject.win32tz import Win32tz
 
 
-@skipUnless(sys.platform == "win32", "This is windows specific module")
-class TestWin32tz(TestCase):
-    def setUp(self):
-        self.local = Win32tz("Central Standard Time")  # pylint:disable=E0601,E0606
+@pytest.mark.skipif(sys.platform != "win32", reason="This is a windows-specific module")
+class TestWin32tz:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        # pylint:disable=E0601,E0606,W0201
+        self.local = Win32tz("Central Standard Time")
         self.oct1 = dt.datetime(month=10, year=2004, day=1, tzinfo=self.local)
         self.dec1 = dt.datetime(month=12, year=2004, day=1, tzinfo=self.local)
         self.braz = Win32tz("E. South America Standard Time")
 
     # TODO: test case taken from doctest, need help.
-    @skip("It fails, need debugging")
+    @pytest.mark.skip(reason="This test fails and needs debugging")
     def test_dst_braz(self):
-        self.assertEqual(self.braz.dst(self.oct1), dt.timedelta(0))
-        self.assertEqual(self.braz.dst(self.dec1), dt.timedelta(seconds=3600))
+        assert self.braz.dst(self.oct1) == dt.timedelta(0)
+        assert self.braz.dst(self.dec1) == dt.timedelta(seconds=3600)
 
     def test_dst_local(self):
-        self.assertEqual(self.oct1.dst(), dt.timedelta(seconds=3600))
-        self.assertEqual(self.dec1.dst(), dt.timedelta(0))
+        assert self.oct1.dst() == dt.timedelta(seconds=3600)
+        assert self.dec1.dst() == dt.timedelta(0)
